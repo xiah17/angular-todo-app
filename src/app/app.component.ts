65a9b26fb52816ad82e96a00ad57eaf7c1824f77
xiah17 <Jalen.1720@gmail.com>
2025-08-24 15:54:11 +0800

    Finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NgClass, NgFor } from '@angular/common';
+
+export interface TodoItem {
+  id: number;
+  task: string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [RouterOutlet, NgFor, NgClass]
 })
+
 export class AppComponent {
-  title = 'angular-todo-app';
-}
+
+  todoList: TodoItem[] = [];
+    newTask: string = '';
+    @ViewChild('todoText') todoInputRef: ElementRef<HTMLInputElement> = null!;
+
+    ngOnInit(): void {
+      const storedTodoList = this.todoList;
+      //  if(storedTodoList) {
+      //    this.todoList = JSON.parse(storedTodoList);
+      //  }
+    }
+    
+    addTask(text: string): void {
+      if (text.trim() !== '') {
+        const newTodoItem: TodoItem = {
+          id: Date.now(),
+          task: text.trim(),
+          completed: false,
+          
+        };
+        this.todoList.push(newTodoItem);
+        this.todoInputRef.nativeElement.value = '';
+        this.saveTodoList();
+      }
+    } 
+    
+    deleteTask(id: number): void {
+      this.todoList = this.todoList.filter(item => item.id !== id);
+      this.saveTodoList();
+    }
+    
+    toggleCompleted(id: number): void {
+      const todoItem = this.todoList.find(item => item.id === id);
+      if (todoItem) {
+          todoItem.completed = !todoItem.completed;
+          this.saveTodoList();
+      }
+    }
+
+    saveTodoList(): void {
+      // localStorage.setItem('todoList', JSON.stringify(this.todoList));
+    }
+
+  }
+
+
